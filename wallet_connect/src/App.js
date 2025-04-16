6f6d748a4d2b0d7d2431ebca1b05b589e8b63b6f
mohammad-pourbafrani <m.pourbafarani@gmail.com>
2025-04-16 19:14:20 +0330

    complete

--- wallet_connect/src/App.js
+++ wallet_connect/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import { createThirdwebClient } from "thirdweb";
+import { ThirdwebProvider, ConnectButton, ConnectEmbed } from "thirdweb/react";
+import { createWallet } from "thirdweb/wallets";
+
+const client = createThirdwebClient({ clientId: "86c3b16a19c7a5887ff4226b0c38c9d0" , secretKey: "SEoMOkc3ag7KlkXxhUhI4EYwmTf9Z0a8ws1GU_9YkW1sRjGOtWI5ius9qa13OjHTZ14-RiqyBkKWgAo4piymuA" });
+
+const wallets = [
+  createWallet("io.metamask"),
+  createWallet("walletConnect"),
+  createWallet("io.hippowallet"),
+];
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThirdwebProvider>
+      <ConnectButton
+        client={client}
+        wallets={wallets}
+      />
+    </ThirdwebProvider>
   );
 }
 
-export default App;
